767ca650f6f5eb45d98d9b64e0282ef3dd731bc3
Israel <brayan@israel003gmail.com>
2021-09-16 15:09:41 -0500

    Actividad 5

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+     public static void main(String[] args) {
+         System.out.println("Actividad 5!");
+         int x=86, y=38;
+         int suma=x+y;
+         int resta=x-y;
+         int multi=x*y;
+         int division=x/y;
+         int residuo=x%y;
+         System.out.println("X= "+x);
+         System.out.println("Y= "+y);
+         System.out.println("Suma= "+suma);
+         System.out.println("Resta= "+resta);
+         System.out.println("Multiplicación= "+multi);
+         System.out.println("División= "+division);
+         System.out.println("Módulo= "+residuo);
+    }
+}
